6797d3adf07ba70883c89e63ba2512cde4b071fd
Adam <129908610+adampdxdotcom@users.noreply.github.com>
2025-11-01 15:56:54 -0700

    Create menu.php

---
+++ admin/menu.php
@@ -0,0 +1,23 @@
+<?php
+// admin/menu.php
+
+// If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Adds the "TW Scripts" menu item to the WordPress admin sidebar.
+ */
+function tw_scripts_add_admin_menu() {
+    add_menu_page(
+        'TW Scripts Viewer',        // The text for the page's <title> tag
+        'TW Scripts',               // The text displayed in the menu
+        'manage_options',           // The capability required to see this menu (Admins only)
+        'tw-scripts-viewer',        // The unique slug for this menu page
+        'tw_scripts_render_viewer_page', // The function that will render the page's content (we will create this in the next step)
+        'dashicons-media-code',     // The icon to display
+        80                          // The position in the menu order
+    );
+}
+add_action( 'admin_menu', 'tw_scripts_add_admin_menu' );
