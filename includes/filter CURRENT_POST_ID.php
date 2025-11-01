bd7cf9aa3134d013bc8aca7744b47b1f985e84b4
Adam <129908610+adampdxdotcom@users.noreply.github.com>
2025-11-01 15:01:11 -0700

    Create filter CURRENT_POST_ID.php

---
+++ includes/filter CURRENT_POST_ID.php
@@ -0,0 +1,13 @@
+add_filter( 'render_block', function( $block_content, $block ) {
+    // Only act on Shortcode block and when token present
+    if ( isset( $block['blockName'] ) && $block['blockName'] === 'core/shortcode'
+         && strpos( $block_content, '%CURRENT_POST_ID%' ) !== false ) {
+
+        // get_the_ID() is set to the Query Loop item while inner blocks render
+        $loop_id = get_the_ID();
+        if ( $loop_id ) {
+            $block_content = str_replace( '%CURRENT_POST_ID%', intval( $loop_id ), $block_content );
+        }
+    }
+    return $block_content;
+}, 10, 2 );
